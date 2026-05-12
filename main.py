7a4640de27b12047771b66af82f31b32dde9e00e
Benedikt Brickwell <brickwellbenedikt@gmail.com>
2026-05-12 13:10:27 +0200

    update plan

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ def main():
     # Spiellogik inplementieren
     pass
 
+    #Todo: Auswahl des Spielsmodus (PvP oder PvE)
 
+    #Todo: Spielfeld erstellen
+
+    #Todo: Setzen der Schiffe
+
+    #Todo: Zug für Zug Schießen
+        # Spiellogik checkt ob ein schuss erfolgreich war
+
+    # Wenn alle Schiffe zerstört sind, sagen welcher Spieler gewonnen hat
 
 
 if __name__ == "__main__":
